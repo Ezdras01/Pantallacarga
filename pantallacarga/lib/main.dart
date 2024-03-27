8430e9a08d93a5344740c2626b531038e7174477
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-26 20:26:13 -0600

    Circulo de carga
    se agrego un circulo de carga sencillo para el "hello world"

--- pantallacarga/lib/main.dart
+++ pantallacarga/lib/main.dart
@@ -4,16 +4,35 @@ void main() {
   runApp(const MainApp());
 }
 
+Future<String> loadData() async {
+  // Simula una operación de carga con un delay
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Data Loaded';
+}
+
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: FutureBuilder<String>(
+        future: loadData(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: Text('Hello World!'),
+              ),
+            );
+          }
+        },
       ),
     );
   }
